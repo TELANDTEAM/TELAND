8ca135297261f6d796408962ff7a0ce63bce1d6d
TELANDTEAM <82978925+TELANDTEAM@users.noreply.github.com>
2021-05-05 04:44:45 +0300

    Add files via upload

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-cd $HOME/TELAND
+cd $(cd $(dirname $0); pwd)
 install() {
 rm -rf $HOME/.telegram-cli
 sudo chmod +x tg
@@ -8,6 +8,25 @@ chmod +x Run
 ./Run
 }
 installall(){
+Version=`lsb_release -rs | cut -f1 -d"."`
+if [ "$Version" == "18" ];then
+echo -e "Installing depedencies for ubuntu 18  ... \n"
+cd /lib/x86_64-linux-gnu/ && sudo ln -s libreadline.so.7.0 libreadline.so.6
+wget "http://black-source.tk/BlackTeAM/Api/compat-libevent2-5_2.0.21-1ubuntu18_amd64-1.deb" && sudo dpkg -i compat-libevent2-5_2.0.21-1ubuntu18_amd64-1.deb
+rm compat-libevent2-5_2.0.21-1ubuntu18_amd64-1.deb
+fi
+if [ "$Version" == "20" ];then
+echo -e "Installing depedencies for ubuntu 20  ... \n"
+cd /lib/x86_64-linux-gnu/ && sudo ln -s libreadline.so.7.0 libreadline.so.6
+wget "http://black-source.tk/BlackTeAM/Api/tmux_3.0a-2ubuntu0.2_amd64.deb" && sudo dpkg -i tmux_3.0a-2ubuntu0.2_amd64.deb
+rm tmux_3.0a-2ubuntu0.2_amd64.deb
+fi
+if [ "$Version" == "21" ];then
+echo -e "Installing depedencies for ubuntu 21  ... \n"
+cd /lib/x86_64-linux-gnu/ && sudo ln -s libreadline.so.7.0 libreadline.so.6
+wget "http://black-source.tk/BlackTeAM/Api/tmux_3.1c-1_amd64.deb" && sudo dpkg -i tmux_3.1c-1_amd64.deb
+rm tmux_3.1c-1_amd64.deb
+fi
 sudo apt-get update -y
 sudo apt-get upgrade -y
 sudo apt-get install software-properties-common -y
